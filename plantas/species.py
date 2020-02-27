aaa2c39063f5633e757a8f50a372a430bea73375
Sergio Enriquez <eqz.nava@gmail.com>
2020-02-27 10:31:19 -0600

    First commit

---
+++ plantas/species.py
@@ -0,0 +1,12 @@
+import uuid
+
+class Plants:
+
+    def __init__(self, quantity, name, scientific_name, type_of_plant, irrigation, season, pui=None):
+        self.quantity = quantity
+        self.name = name
+        self.scientific_name = scientific_name
+        self.type_of_plant = type_of_plant
+        self.irrigation = irrigation
+        self.season = season
+        self.pui = pui or uuid.uuid4()
